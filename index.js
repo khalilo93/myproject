e43d61a17894fc656dba2dbd9a2b0b67194a1c1d
khalilo93 <khalil.mahjoub@hotmail.fr>
2024-08-20 14:24:10 +0100

    myproject

--- index.js
+++ index.js
@@ -16,7 +16,43 @@ function createvideogame(name,category,developors,releaseDate,price,image){
         id:id()
     }
 }
-var game1=createvideogame(
+
+function Makevideogame(initial){
+    return{
+        name:initial,
+        list:[],
+        add:add,
+        display:display,
+        remove:remove
+    }
+}
+var add = function(name,category,developors,releaseDate,price,image){
+    this.list.push(createvideogame(name,category,developors,releaseDate,price,image,id))
+}
+
+
+var display = function(){
+    console.log("ggg",this.list)
+    for (let i = 0; i < this.list.length; i++) {
+       
+        var c=$("<div></div>")
+        
+
+        c.append(`<div><p>name:${this.list[i].name}</p></div>`)
+        c.append(`<div><p>category:${this.list[i].category}</p></div>`)
+        c.append(`<div><p>developors:${this.list[i].developors}</p></div>`)
+        c.append(`<div><p>releaseDate:${this.list[i].releaseDate}</p></div>`)
+        c.append(`<div><p>price:${this.list[i].price}</p></div>`)
+        c.append(`<div><img src="${this.list[i].image}"></div>`)
+        $('body').append(c) 
+    }
+}
+var remove=function(id){
+    this.list.splice(id,1)
+  }
+var x=Makevideogame()
+
+x.add(
     "League Of Legends",
     "MOBA",
     "Riot games",
@@ -25,4 +61,45 @@ var game1=createvideogame(
     "https://cdn1.epicgames.com/offer/24b9b5e323bc40eea252a10cdd3b2f10/EGS_LeagueofLegends_RiotGames_S1_2560x1440-80471666c140f790f28dff68d72c384b"
 
 )
-var game2=createvideogame
+x.add(
+    "Assassin's Creed Valhalla",
+    "adventure",
+    "Ubisoft",
+    "10 novembre 2020",
+    20,
+    "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6ZAlQrGYxXi8Bo8PuhJd5g/206c57ecb27e58b4555089c6c80d3db5/ACV_meta.png"
+)
+x.add(
+    "Fortnite",
+    "royal battle",
+    "Epic games",
+    "21 juillet 2017",
+    0,
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtS4sZv_gqhio9QZzxMRwwuCzSyWUiqZxAvw&s"
+)
+x.add(
+    "Valorant",
+    "FPS",
+    "Riot games",
+    "2 juin 2020",
+    0,
+    "https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2020/6/5/ctsejxmdtw9inp8zqqqd/valorant-ameliorer-aim-visee-astuces"
+)
+x.add(
+    "Hearthstone",
+    "card game",
+    "Blizzard",
+    "11 mars 2014",
+    0,
+    "https://blz-contentstack-images.akamaized.net/v3/assets/bltc965041283bac56c/blt82bb2ee72a2b0baf/651ee3b3e4a8af6535bd6de3/stepintavernthumbnail.PNG"
+)
+x.add(
+    "The Legend of Zelda",
+    "adventure",
+    "Nintendo",
+    "21 février 1986",
+    50,
+ 
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpqsNiv56Y5FNp6VgFHzpHqH5Aa58lm2-8Bg&s"
+)
+x.display()
